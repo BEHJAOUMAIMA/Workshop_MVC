c126236033850d41f4c893a7205bbcac140220a1
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-01-27 17:12:22 +0100

    ajout du datatable

---
+++ MVCCRUD/JokeList/View.php
@@ -0,0 +1,21 @@
+<div class="table-responsive mx-auto">
+    <table id="dataTableId" class="display table table-light table-hover" style="width:100%">
+        <thead>
+            <tr>
+                <th scope="col">N°</th>
+                <th scope="col">Blague</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr> 
+                <th scope="row"></th>
+                <td></td>
+                    <td class="d-flex">
+                        <button type="button" class="btn mx-1" style="background-color: #3C6255;color: white;" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="bi bi-pencil-square"></i></button>
+                        <button type="button" class="btn" style="background-color: #8E415B;color: white;" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="bi bi-trash3"></i></button>
+                    </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
--- MVCCRUD/public/index.php
+++ MVCCRUD/public/index.php
@@ -8,11 +8,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <!-- links bootstrap end -->
+    <!-- links css -->
     <link rel="stylesheet" href="../../assets/style.css">
+    <!-- links datatable -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
     <title>Blagues</title>
 </head>
 <body>
-    <div class="container">
+    <div class="row">
         <div class="intro bg-dark d-flex flex-column text-center mx-auto mt-5 py-3 rounded-3 text-white">
             <p class="fs-3 fw-bold">Rire ça fait du bien et pour rire il faut déjà se sentir bien. </p>
             <p class="fs-4">Créer vos propres blagues  <i class="bi bi-emoji-laughing"></i> !</p>
@@ -21,10 +24,23 @@
                 <i class="bi bi-plus-lg me-1"></i> Ajouter une blague
             </button>
         </div>
-        <?php
+    </div>
+    <?php
             require "../JokeForm/View.php";
         ?>
+    <div class="mt-5 w-75 mx-auto">
+        <?php
+            require "../JokeList/View.php";
+        ?>  
     </div>
 </body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#dataTableId').DataTable();
+    });
+</script>
 </html>
